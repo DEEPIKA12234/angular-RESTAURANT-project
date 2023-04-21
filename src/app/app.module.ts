e4282891e27ed71b343875718d93abc90a587867
deepika <deepikakadam8848@gmail.com>
2023-04-21 14:57:07 +0530

    secondcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { MenupageComponent } from './menupage/menupage.component';
+import { RouterModule, Routes } from '@angular/router';
+import { RatingComponent } from './rating/rating.component';
+import { OrderdetailsService } from './orderdetails.service';
+import { SearchComponent } from './search/search.component';
+import { FormsModule } from '@angular/forms';
+
+const routes:Routes=[
+  {path:"",component: HomeComponent},
+  {path:"menu",component: MenuComponent},
+  {path:"menupage/:id", component:MenupageComponent},
+  {path:"about", component:AboutComponent},
+  {path:"contact", component:ContactComponent},
+  {path:"**", redirectTo:"",pathMatch:'full'}
+
+  ]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    MenuComponent,
+    AboutComponent,
+    ContactComponent,
+    MenupageComponent,
+    RatingComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RouterModule.forRoot(routes) , FormsModule
   ],
-  providers: [],
+  providers: [OrderdetailsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
